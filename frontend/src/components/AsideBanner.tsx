3f1f0e204a1814c877c03a245a7c4bb48f404f55
Yuri Alves <yuripiresalves@gmail.com>
2023-01-30 14:22:09 -0300

    tela login

---
+++ frontend/src/components/AsideBanner.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+// import AppPreviewImg from '../../public/appMockup.png';
+import { Logo } from './Logo';
+
+export function AsideBanner() {
+  return (
+    <aside className="hidden bg-emerald-800 md:flex flex-col flex-1 items-center justify-between p-8 text-zinc-200">
+      <main className="flex flex-col items-center">
+        <Logo />
+        <p className="text-center my-6">
+          Organize seus animes preferidos em listas
+        </p>
+        {/* <Image
+          src={AppPreviewImg}
+          alt="Imagem mostrando o app Listit em um celular, notebook e tablet"
+          width={320}
+          priority
+          quality={100}
+          className="2xl:mt-10 2xl:w-full"
+        /> */}
+      </main>
+      <footer>
+        <span>© Todos os direitos reservados.</span>
+      </footer>
+    </aside>
+  );
+}
