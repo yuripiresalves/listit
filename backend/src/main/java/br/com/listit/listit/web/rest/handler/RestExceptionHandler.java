83d2f71024d0bcd16e32fe45c5a320c29f730fa0
Nayane <nayanebatistaa@gmail.com>
2023-02-05 23:43:44 -0300

    Implementação do requisito R04 - Operações de exibir detalhes

---
+++ backend/src/main/java/br/com/listit/listit/web/rest/handler/RestExceptionHandler.java
@@ -0,0 +1,62 @@
+package br.com.listit.listit.web.rest.handler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.listit.listit.services.remote.exceptions.BadRequestClientServiceException;
+import lombok.Builder;
+import lombok.Data;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(BadRequestClientServiceException.class)
+    public ResponseEntity<BadRequestExceptionDetails> badRequestClientServiceException(BadRequestClientServiceException badRequestException){
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                .timeStamps(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title("Bad Request Exception, try again")
+                .details(badRequestException.getMessage())
+                .developerMessge(badRequestException.getClass().getName())
+                .build(), HttpStatus.BAD_REQUEST
+        );
+    }
+	
+    @Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+			HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+    	return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                .timeStamps(LocalDateTime.now())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .title("Error Internal")
+                .details(ex.getMessage())
+                .developerMessge(ex.getClass().getName())
+                .build(), HttpStatus.INTERNAL_SERVER_ERROR
+        );
+	}
+
+	@Builder
+	@Data
+	private static class BadRequestExceptionDetails{
+	    protected String title;
+	    protected int status;
+	    protected String details;
+	    protected String developerMessge;
+	    @JsonFormat(pattern = "dd/MM/yyyy hh:mm:ss a")
+	    protected LocalDateTime timeStamps;
+
+	}
+
+}
