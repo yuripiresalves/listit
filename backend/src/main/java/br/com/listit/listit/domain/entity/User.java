2b13d3c1dd867371f8cbee03df8b875e8fbe98a7
Nayane <nayanebatistaa@gmail.com>
2023-03-05 21:39:55 -0300

    R04 - Refatorar trechos do backend

--- backend/src/main/java/br/com/listit/listit/domain/entity/User.java
+++ backend/src/main/java/br/com/listit/listit/domain/entity/User.java
@@ -1,13 +1,12 @@
 package br.com.listit.listit.domain.entity;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
