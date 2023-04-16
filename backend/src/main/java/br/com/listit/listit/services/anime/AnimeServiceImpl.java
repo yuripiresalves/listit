bf471acd2ccd9962dec0acde942eb9a1b65c63fe
Yuri Alves <yuripiresalves@gmail.com>
2023-04-16 17:11:02 -0300

    refactory tests

--- backend/src/main/java/br/com/listit/listit/services/anime/AnimeServiceImpl.java
+++ backend/src/main/java/br/com/listit/listit/services/anime/AnimeServiceImpl.java
@@ -17,59 +17,56 @@ import net.sandrohc.jikan.model.anime.Anime;
 @AllArgsConstructor
 @Data
 public class AnimeServiceImpl implements AnimeService {
-	
+
 	private ClientRemoteApiJikan clientRemoteApiJikan;
 
 	@Override
 	public List<AnimeRecord> findAnimeByTitle(String title) {
 		List<Anime> searchByName = clientRemoteApiJikan.searchByName(title);
-		
-		if(searchByName == null) {
+
+		if (searchByName == null) {
 			throw new BadRequestClientServiceException("not found animes");
 		}
-		
-		List<AnimeRecord> animeRecordList = searchByName.stream().map(s -> convertAnimeToAnimeRecordEntity(s)).collect(Collectors.toList());
+
+		List<AnimeRecord> animeRecordList = searchByName.stream().map(s -> convertAnimeToAnimeRecordEntity(s))
+				.collect(Collectors.toList());
 		return animeRecordList;
 	}
-	
+
 	public AnimeRecord findAnimeByID(int id) {
 		Anime searchById = clientRemoteApiJikan.searchById(id);
-		
-		if(searchById == null) {
+
+		if (searchById == null) {
 			throw new BadRequestClientServiceException("Erro anime not found");
 		}
-		
+
 		return convertAnimeToAnimeRecordEntity(searchById);
 	}
 
 	private AnimeRecord convertAnimeToAnimeRecordEntity(Anime anime) {
 		ImageJPG imageJpgConverted = null;
-		if(anime.getImages() != null) {
+		if (anime.getImages() != null) {
 			imageJpgConverted = convertImageJPGToImageEntity(anime.getImages().jpg);
 		}
-		
-		if(anime.getImages()!=null) {
-			imageJpgConverted = convertImageJPGToImageEntity(anime.getImages().jpg);
-		}
-		 
+
 		return AnimeRecord.builder()
 				.id(anime.getMalId())
 				.title(anime.getTitle())
 				.titleEnglish(anime.getTitleEnglish())
 				.titleJapanese(anime.getTitleJapanese())
-				.episodes(anime.getEpisodes() == null ? 0:anime.getEpisodes())
+				.episodes(anime.getEpisodes() == null ? 0 : anime.getEpisodes())
 				.score(anime.getScore())
 				.imageJPG(imageJpgConverted)
 				.synopsis(anime.getSynopsis())
 				.build();
 	}
-	
+
 	private ImageJPG convertImageJPGToImageEntity(net.sandrohc.jikan.model.common.Image jpg) {
 		ImageJPG image = ImageJPG.builder()
-			.imageURL(jpg.getImageUrl())
-			.smallImageURL(jpg.smallImageUrl)
-			.largeImageURL(jpg.largeImageUrl)
-			.build();
+				.imageURL(jpg.getImageUrl())
+				.smallImageURL(jpg.smallImageUrl)
+				.largeImageURL(jpg.largeImageUrl)
+				.build();
 		return image;
 	}
 
